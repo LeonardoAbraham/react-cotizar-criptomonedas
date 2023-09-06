5f4013cd18a60e60c191f3663eba7f7014dbc3b0
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-06 16:27:12 -0600

    Styled Components al Select

--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -3,6 +3,18 @@ import styled from '@emotion/styled'
 
 const Label = styled.label`
     color: #FFF;
+    display: block;
+    font-family: 'Lato', sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`
+
+const Select =styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
 `
 
 const useSelectMonedas = (label, opciones) => {
@@ -10,7 +22,7 @@ const useSelectMonedas = (label, opciones) => {
     const SelectMonedas = () => ( 
         <>
             <Label>{label}</Label>
-            <select>
+            <Select>
                 <option value="">Seleccione</option>
 
                 {
@@ -23,7 +35,7 @@ const useSelectMonedas = (label, opciones) => {
                         </option>
                     ))
                 }
-            </select>
+            </Select>
         </>
     )
 
